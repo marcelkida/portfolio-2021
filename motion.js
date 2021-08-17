8caa05fe08378589db0f48050b2a9e6b2c36212e
Marcel Kida <kida.marcel@gmail.com>
2021-08-17 08:15:42 -0700

    motion

---
+++ motion.js
@@ -0,0 +1,14 @@
+new Freezeframe('.freezeframe', {
+    trigger: 'hover',
+    overlay: false
+  });
+  /*
+  $(document).ready(function() {
+    $(".myvideos").on("mouseover", function(event) {
+      this.play();
+  
+    }).on('mouseout', function(event) {
+      this.pause();
+    });
+  }
+  */
